45ea89c88b999fcb044f08076d85b3ba311f0a5a
lining1884 <39609282+lining1884@users.noreply.github.com>
2018-09-28 14:00:34 +0800

    create financial system for qianchencompany
    create financial system for qianchencompany

---
+++ Qcms/Qcms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Qcms.Startup))]
+namespace Qcms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
